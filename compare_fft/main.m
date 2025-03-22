2180768e95cca00b61eeaa894f38e541460e9cb0
juloeng <julesgallien163@gmail.com>
2025-03-22 15:19:30 +0100

    feat: adding a function to generate signals

--- compare_fft/main.m
+++ compare_fft/main.m
@@ -2,3 +2,23 @@
 % -- Description : Compare two signals using FFT (Fast Fourier Transform)   -- %
 % -- Author      : juloeng                                                  -- %
 % ---------------------------------------------------------------------------- %
+
+% This function returns one clean signal and one noisy signal
+[t, clean_signal, noisy_signal] = gen_signals();
+
+% -- Plot
+figure;
+subplot(2,1,1);
+plot(t, clean_signal);
+title('Clean signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+ylim([-1.5 1.5])
+
+subplot(2,1,2);
+plot(t, noisy_signal);
+title('Noisy signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+ylim([-2 2]);
+
